9b43c62393894d9209ffe33356d2ab87ffa9499c
rbsamiur <rbsamiur@gmail.com>
2022-10-03 05:54:03 +0600

    String Mistake Fixed

--- reviews/urls.py
+++ reviews/urls.py
@@ -6,10 +6,10 @@ app_name = 'reviews'
 
 urlpatterns = [
     
-    path('list/',views.review_list , name='createBlog'),
-    path('create/',views.create_review , name='createBlog'),
-    path('update/<int:pk>',views.update_review , name='updateBlog'),
-    path('delete/<int:pk>',views.delete_review , name='deleteBlog'),
+    path('list/',views.review_list , name='getReview'),
+    path('create/',views.create_review , name='createReview'),
+    path('update/<int:pk>',views.update_review , name='updateReview'),
+    path('delete/<int:pk>',views.delete_review , name='deleteReview'),
     
      
 ]
